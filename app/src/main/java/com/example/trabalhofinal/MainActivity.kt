5ba3a50e4e3092a05cd61df4843f43380e7f7fff
Lavínia Barbosa Dantas de Souza <laviniabd.barbosa@gmail.com>
2025-09-10 14:01:56 -0300

    scaffold na home

--- app/src/main/java/com/example/trabalhofinal/MainActivity.kt
+++ app/src/main/java/com/example/trabalhofinal/MainActivity.kt
@@ -1,5 +1,4 @@
 package com.example.trabalhofinal
-import android.R.attr.onClick
 import android.app.Application
 import android.os.Bundle
 import androidx.activity.ComponentActivity
@@ -8,27 +7,28 @@ import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
-import androidx.compose.material3.Button
+import androidx.compose.material3.BottomAppBar
 import androidx.compose.material3.Card
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults.topAppBarColors
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
@@ -39,7 +39,6 @@ import com.example.aula17.data_layer.Disciplina
 import com.example.aula17.data_layer.DisciplinaComNotas
 import com.example.aula17.data_layer.DisciplinaRepository
 import com.example.aula17.data_layer.TrabFinalDatabase
-import com.example.trabalhofinal.ui.theme.TrabalhoFinalTheme
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.flow.update
@@ -50,7 +49,7 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            MinhaTela()
+            Home()
         }
     }
 }
@@ -111,17 +110,19 @@ fun ListaDisciplinas(
             listaDisciplinas.forEach { element ->
                 Card(
                     modifier = Modifier
+                        .padding(bottom = 16.dp)
                         .fillMaxWidth()
-                        .padding(bottom = 10.dp)
                 ) {
                     Row(
                         verticalAlignment = Alignment.CenterVertically,
                         horizontalArrangement = Arrangement.SpaceBetween,
                         modifier = Modifier
+//                            .background(color = MaterialTheme.colorScheme.primaryContainer)
                             .fillMaxWidth()
-                            .padding(start = 10.dp),
+                            .padding(start = 10.dp)
+                        ,
                     ) {
-                        Text(text = element.disciplina.nome)
+                        Text(text = element.disciplina.nome, style = MaterialTheme.typography.titleMedium, modifier = Modifier.padding(10.dp))
                     }
                 }
             }
@@ -129,46 +130,50 @@ fun ListaDisciplinas(
     }
 }
 
-@Preview
-@Composable
-fun MinhaTela(meuViewModel: MeuViewModel = viewModel()) {
-    val uiState by meuViewModel.uiState.collectAsStateWithLifecycle()
 
-    TrabalhoFinalTheme {
-        Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-            Column(
-                modifier = Modifier
-                    .padding(innerPadding)
-                    .padding(16.dp)
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun Home(meuViewModel: MeuViewModel = viewModel()) {
+    val uiState by meuViewModel.uiState.collectAsStateWithLifecycle()
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                colors = topAppBarColors(
+                    containerColor = MaterialTheme.colorScheme.primaryContainer,
+                    titleContentColor = MaterialTheme.colorScheme.primary,
+                ),
+                title = {
+                    Text("Lista de Disciplinas")
+                }
+            )
+        },
+        bottomBar = {
+            BottomAppBar(
+                containerColor = MaterialTheme.colorScheme.primaryContainer,
+                contentColor = MaterialTheme.colorScheme.primary,
             ) {
                 Text(
-                    text = "Lista de Disciplinas",
-                    style = MaterialTheme.typography.titleLarge,
-                    modifier = Modifier.align(Alignment.CenterHorizontally)
+                    modifier = Modifier
+                        .fillMaxWidth(),
+                    textAlign = TextAlign.Center,
+                    text = "Aq vai ficar nossa barra de navegacao",
                 )
-
-                Spacer(modifier = Modifier.height(16.dp))
-
-                // Botão para inserir um novo paciente
-
-                    FloatingActionButton(
-                        onClick = { meuViewModel.insereDisciplina() },
-                    ) {
-                        Icon(Icons.Filled.Add, "Floating action button.")
-                    }
-
-//                Button(
-//                    onClick = { meuViewModel.insereDisciplina() },
-//                    modifier = Modifier.fillMaxWidth()
-//                ) {
-//                    Text(text = "Adicionar Nova Disciplina")
-//                }
-
-                Spacer(modifier = Modifier.height(16.dp))
-
-                // Exibe a lista de pacientes
-                ListaDisciplinas(listaDisciplinas = uiState.lista)
             }
+        },
+        floatingActionButton = {
+            FloatingActionButton(onClick = { meuViewModel.insereDisciplina() }) {
+                Icon(Icons.Default.Add, contentDescription = "Add")
+            }
+        }
+    ) {  innerPadding ->
+        Column(
+            modifier = Modifier
+                .padding(innerPadding)
+                .padding(20.dp)
+        ) {
+
+            ListaDisciplinas(listaDisciplinas = uiState.lista)
         }
     }
 }
